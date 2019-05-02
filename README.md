e463481c253935f9bbf6fbd9512f416ee48e97c1
Carlos Libero <clibero@gmail.com>
2019-05-02 12:09:44 -0300

    feat: Implemented my own game images, style and fonts
    It was implemented my own set of colors and fonts to the game. It was
    also implemented rock style and images and the shuffle functionality.
    
    As now, games has an array of 16 card elements with rock images. But
    no click action is added to it, meaning that nothing can be done with
    the game yet.

--- README.md
+++ README.md
@@ -23,3 +23,32 @@ For specific, detailed instructions, look at the project instructions in the [Ud
 
 As this projects aims to pass in Udacity project 2 only, it is closed for contribution.
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## Acknowledgments
+
+- First full project combining HTML5, CSS3 and JavaScript ES6
+- Learned how events integrate to webpage
+- Experienced simple game logic
+
+## Project details
+
+This project started with a provided mockup page and already some coding on it. This turns to be a challenge and it gives some sort of a path to follow but adds the need to understand other person view and coding style to the problem.
+Thus, the following actions were performed in this provided order:
+1. Understood provided logic
+1. Implemented my own design to game, with colors, fonts and images
+1. Created a deck of cards using the array approach
+1. Used provided shuffle function to array
+1. Added reload functionality (TBD)
+1. Added click functionalities (TBD)
+  1. flip (TBD)
+  1. flip animation (TBD)
+  1. unflip (TBD)
+  1. matched (TBD)
+  1. matched animation (TBD)
+  1. not matched (TBD)
+  1. not matched animation (TBD)
+  1. Limit to two cards flipped only (TBD)
+  1. Move counter (TBD)
+1. Created game win condition and modal window (TBD)
+1. Implemented game clock (TBD)
+1. Implemented game stars logic (3 stars based on number of moves) (TBD)
--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 /*
  * Create a list that holds all of your cards
- */
+* Will use list from https://www.ranker.com/crowdranked-list/the-best-rock-bands-of-all-time
+* As it is a 16 memory game, the best 8 rock bands will be used
+*/
 
+// All 16 cards will be stored in cardsArray for usage in other parts of game
+let cardsArray = ['Led Zeppelin', 'Led Zeppelin', 'The Beatles', 'The Beatles',
+                  'Pink Floyd', 'Pink Floyd', 'The Rolling Stones', 'The Rolling Stones',
+                  'Queen', 'Queen', 'The Who', 'The Who',
+                  'ACDC', 'ACDC', 'Jimi Hendrix', 'Jimi Hendrix'];
+
+// Array that will handle cards that got clicked
+let cardsClickedArray = [];
+
+// Array that will have document elements for all cards objects
+const cards = document.querySelectorAll('.image');
 
 /*
  * Display the cards on the page
@@ -9,6 +22,66 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+//Initialize game
+gameInit()
+
+
+// Game Initialization function to initialize game logic as needed
+function gameInit() {
+  // Shuffles cardsArray
+  shuffle(cardsArray);
+
+  // Calls update function to update images in proper card divs
+  updateCards(cards);
+
+}
+
+// Return image name according to value informed
+function imageToUse (imageText) {
+  // Based on array text returns the image name and path to be used.
+  switch (imageText) {
+    case 'Led Zeppelin':
+      return('img/led-zeppelin-photo-u16.webp');
+      break;
+    case 'The Beatles':
+      return('img/the-beatles-writers-photo-1.webp');
+      break;
+    case 'Pink Floyd':
+      return('img/pink-floyd-recording-artists-and-groups-photo-2.webp');
+      break;
+    case 'The Rolling Stones':
+      return('img/the-rolling-stones-recording-artists-and-groups-photo-u6.webp');
+      break;
+    case 'Queen':
+      return('img/queen-photo-u13.webp');
+      break;
+    case 'The Who':
+      return('img/the-high-numbers-writers-photo-2.webp');
+      break;
+    case 'ACDC':
+      return('img/ac-and-dc-recording-artists-and-groups-photo-u6.webp');
+      break;
+    case 'Jimi Hendrix':
+      return('img/jimi-hendrix-recording-artists-and-groups-photo-u36.webp');
+      break;
+    default:
+  }
+}
+
+// Update cards based on provided Array
+function updateCards(arrayCardElement) {
+  // loop through card divs
+  for (let i=0; i < arrayCardElement.length; i++) {
+    updateCardImageSrc(arrayCardElement[i], i);
+  }
+}
+
+// Update card image src attribute and alt attribute
+function updateCardImageSrc (cardElement, cardItemNumber) {
+  cardElement.src = imageToUse(cardsArray[cardItemNumber]);
+  cardElement.alt = cardsArray[cardItemNumber];
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
